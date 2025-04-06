73ac8666131bd844282b3bd8be2f98deebfb583b
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2025-04-06 07:57:28 +0000

    todo adding

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import TodoItem from "./components/TodoItem";
 import { dummyData } from "./data/Todos";
+import AddTodoForm from "./components/AddTodoForm";
 
 export default function ToDo() {
 
@@ -16,10 +17,27 @@ export default function ToDo() {
         // alert(`Todo with id ${id} is now ${completed ? "completed" : "not completed"}`)
     }
 
+    function addTodo(title: string) {
+        setTodos(prevTodos => [
+            {
+                id: prevTodos.length + 1,
+                title,
+                completed: false
+            },
+            ...prevTodos
+        ])
+    }
+
     return (
         <main className="py-10 h-screen space-y-5 ">
             <h1 className="font-bold text-3xl text-center">RTTD ToDos</h1>
-            <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5" >
+            <div className="max-w-lg mx-auto bg-slate-100 rounded-md p-5 space-y-6" >
+                <div className="space-y-2">
+                    <AddTodoForm
+                        onSubmit={addTodo}
+                    />
+
+                </div>
                 <div className="space-y-2">
                     {todos.map(todo => (
                         <TodoItem
