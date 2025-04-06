6a21903369fb75fab8a03e84bd739ce982f1548f
lordofthemind <70771838+lordofthemind@users.noreply.github.com>
2025-04-06 13:20:35 +0000

    being stored in local storage

--- src/Todo.tsx
+++ src/Todo.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { dummyData } from "./data/Todos";
 import AddTodoForm from "./components/AddTodoForm";
 import ToodoList from "./components/TodoList";
 import TodoSummary from "./components/TodoSummary";
+import { Todo } from "./types/todo";
 
 export default function ToDo() {
 
-    const [todos, setTodos] = useState(dummyData)
+    const [todos, setTodos] = useState(() => {
+        const savedTodos: Todo[] = JSON.parse(localStorage.getItem("todos") || "[]");
+        return savedTodos.length > 0 ? savedTodos : dummyData
+    })
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos])
 
     function setTodoCompleted(id: number, completed: boolean) {
         setTodos(
--- src/data/Todos.ts
+++ src/data/Todos.ts
@@ -1,24 +1,3 @@
 import { Todo } from "../types/todo";
 
-export const dummyData: Todo[] = [
-    {
-    id: 1,
-    title: 'Learn React',
-    completed: false,
-    },
-    {
-    id: 2,
-    title: 'Learn TypeScript',
-    completed: false,
-    },
-    {
-    id: 3,
-    title: 'Learn Tailwind CSS',
-    completed: false,
-    },
-    {
-    id: 4,
-    title: 'Learn React Router',
-    completed: false,
-    },
-];
+export const dummyData: Todo[] = [];
